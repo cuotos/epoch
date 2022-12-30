84fe9beed74770afeab36d76f32bbadd10bf8425
Dan Potepa <dan@danpotepa.co.uk>
2022-12-30 18:24:08 +0000

    added version flag

--- main.go
+++ main.go
@@ -10,7 +10,17 @@ import (
 	"github.com/araddon/dateparse"
 )
 
+var (
+	version = "unset"
+	commit  = "unset"
+)
+
 func main() {
+	if len(os.Args) == 2 && os.Args[1] == "-v" {
+		fmt.Printf("%s-%s", version, commit)
+		os.Exit(0)
+	}
+
 	input := strings.Join(os.Args[1:], " ")
 	parsed, err := dateparse.ParseAny(input)
 	if err != nil {
